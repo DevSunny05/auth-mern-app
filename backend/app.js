892300765e3ed80ed9a59e7dba5e56d1073c8ba1
sarang <sarang.office01@gmail.com>
2022-11-09 21:52:23 +0530

    first commit of this authentication and authorization mern app

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const mongoose=require("mongoose")
+const router=require('./routes/user-routes')
+const cookieParser=require("cookie-parser")
+// express giving its refrance to app variable and app variable holds functionality within application
+const app=express();
+
+// middleware
+app.use(cookieParser());
+app.use(express.json())
+app.use('/api',router)
+
+
+mongoose.connect("mongodb+srv://sarang:sarang@cluster0.be1cpsy.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>{
+    app.listen(5000);
+    console.log("database is connected")
+}).catch((err)=>console.log(err))
---
+++ backend/controllers/user-controller.js
@@ -0,0 +1,110 @@
+const User=require('../model/user')
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+const JWT_SECRET_KEY='Secret Key';
+
+
+const signup=async(req,res,next)=>{
+    const{name,email,password}=req.body;
+    let existingUser;
+
+    try{
+        existingUser=await User.findOne({email:email})
+    }catch(err){
+        console.log(err)
+    }
+
+    if(existingUser){
+        return res.status(400).json({message:"user already exist"})
+    }
+
+    const hashPassword=bcrypt.hashSync(password);
+
+
+    const user=new User({
+        name:name,
+        email:email,
+        password:hashPassword
+    })
+
+    try{
+        await user.save();
+    }catch(err){
+        console.log(err)
+    }
+
+    return res.status(201).json({message:user})
+}
+
+
+const login= async(req,res,next)=>{
+    const {email,password}=req.body;
+    let existingUser;
+
+    try{
+        existingUser=await User.findOne({email:email})
+    }catch(err){
+        return new Error(err)
+    }
+
+    if(!existingUser){
+        return res.status(400).json({message:"user not found"})
+    }
+
+    const isPasswordCorrect= bcrypt.compareSync(password,existingUser.password);
+    if(!isPasswordCorrect){
+        return res.status(400).json({message:"Invalid Email/Password"})
+    }
+
+    const token=jwt.sign({id:existingUser._id},JWT_SECRET_KEY,{
+        expiresIn:'24hr'
+    })
+
+    res.cookie(String(existingUser._id),token,{
+        path:'/',
+        expires:new Date(Date.now()+1000*30),
+        httpOnly:true,
+        sameSite:'lax'
+    })
+
+    return res.status(200).json({message:"successfully login in",user:existingUser,token})
+}
+
+const verifyToken=(req,res,next)=>{
+    const cookies=req.headers.cookie;
+    console.log(cookies)
+    const token=cookies.split("=")[1];
+    console.log(token)
+   
+    if(!token){
+        res.status(404).json({message:"No token found"})
+    }
+    jwt.verify(String(token),JWT_SECRET_KEY,(err,user)=>{
+        if(err){
+            return res.status(400).json({message:"Invalid Token"})
+        }
+        console.log(user)
+        req.id=user.id;
+        next()
+    })
+}
+
+const getUser=async(req,res,next)=>{
+    const userId=req.id;
+    let user;
+    try{
+        user=await User.findById(userId,"-password")
+    }catch(err){
+        return new Error(err)
+    }
+
+    if(!user){
+        return res.status(404).json({message:"No user found"})
+    }
+    return res.status(200).json({user})
+}
+
+exports.signup=signup;
+exports.login=login;
+exports.verifyToken=verifyToken;
+exports.getUser=getUser;
